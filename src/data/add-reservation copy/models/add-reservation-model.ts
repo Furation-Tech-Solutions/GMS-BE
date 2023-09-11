6fe33869e5058ef1650b6406d203fce378770907
satan-furation <137980612+satan-furation@users.noreply.github.com>
2023-09-11 17:51:53 +0530

    fix: bug fix in add reservation and error handling file of type error (#3)

---
+++ src/data/add-reservation copy/models/add-reservation-model.ts
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+
+const addReservationSchema = new mongoose.Schema({
+  //Availability
+  date: {
+    type: String,
+    trim: true,
+  },
+  shift: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Shifts",
+  },
+  duration: {
+    type: String,
+    trim: true,
+  },
+  seatingArea: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SeatingArea",
+  },
+  timeSlot: {
+    type: String,
+    trim: true,
+  },
+  
+  // Client
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+  },
+
+  // Source
+  source: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+  },
+  // Aditional Detail
+  resevationStatus: [{ type: mongoose.Schema.Types.ObjectId, ref: "Users" }],
+  reservationNote: {
+    type: String,
+    maxlength: [2000, "lastName should have less than 30 charcters"],
+    minLength: [1, "lastName should have more than 3 character"],
+    trim: true,
+  },
+
+  table: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Table",
+  },
+  bookedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+  },
+  perks: {
+    type: String,
+    maxLength: [2000, "notes should have less then 500 charcters "],
+    minLength: [5, "notes should have 10 charcters"],
+    trim: true,
+  },
+
+  confirmationMailSending: {
+    type: Boolean,
+    default: false,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const AddReservation = mongoose.model(
+  "AddReservation",
+  addReservationSchema
+);
