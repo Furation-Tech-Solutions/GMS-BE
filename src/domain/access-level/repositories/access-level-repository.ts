8ac6a62b470a6472b8ed542233156674b818857c
Malik04121 <shehzadmalik123.sm@gmail.com>
2023-09-12 12:34:19 +0530

    fix: the issue of git permission

--- src/domain/access-level/repositories/access-level-repository.ts
+++ src/domain/access-level/repositories/access-level-repository.ts
@@ -5,7 +5,6 @@ import { ErrorClass } from "@presentation/error-handling/api-error";
 export interface AccessLevelRepository {
     createAccessLevel(accessLevel: AccessLevelModel): Promise<Either<ErrorClass, AccessLevelEntity>>;
     getAccessLevel():Promise<Either<ErrorClass,AccessLevelEntity[]>>;
-    deleteAccessLevel(id: string): Promise<Either<ErrorClass, void>> 
+    deleteAccessLevel(id: string): Promise<Either<ErrorClass, void>> ;
     getAccessLevelById(id: string): Promise<Either<ErrorClass, AccessLevelEntity>>; 
-
 }
