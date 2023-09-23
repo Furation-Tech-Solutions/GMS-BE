11b7d6e54ff9066aabb424e3cda1a04604f23cc5
satan-furation <satan.sharma@furation.tech>
2023-09-23 15:42:23 +0530

    refactor: populate some felids of client shifts table in add reservation

--- src/presentation/middlewares/add-reservation/add-reservation-validator.ts
+++ src/presentation/middlewares/add-reservation/add-reservation-validator.ts
@@ -10,7 +10,7 @@ interface ReservationInput {
   seatingArea: string;
   timeSlot: string;
   client: string;
-  resevationTags: string[];
+  reservationTags: string[];
   reservationNote: string;
   table: string;
   bookedBy: string;
@@ -69,7 +69,7 @@ const reservationValidator = (
       : Joi.string().trim().required().messages({
           "any.required": "Please select the Client",
         }),
-    resevationTags: Joi.array().items(Joi.string().trim()),
+    reservationTags: Joi.array().items(Joi.string().trim()),
     reservationNote: Joi.string().max(2000).min(1).trim().messages({
       "string.max": "Reservation note should have less than 2000 characters",
       "string.min": "Reservation note should have more than 1 character",
