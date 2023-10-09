5242fd0a6e697a0514bf8504c23f003e347938f6
satan-furation <satan.sharma@furation.tech>
2023-10-09 13:28:26 +0530

    fix: fix cast error in guest and response in array of reservation tag

--- src/data/guest/datasource/guest-datasource.ts
+++ src/data/guest/datasource/guest-datasource.ts
@@ -15,46 +15,46 @@ export class GuestDataSourceImpl implements GuestDataSource {
   constructor(private db: mongoose.Connection) {}
 
   async create(guest: GuestModel): Promise<any> {
-    try {
-      const existingGuest = await Guest.findOne({
-        email: guest.email,
-      }).populate("bookedBy");
-      if (existingGuest) {
-        throw ApiError.guestExist();
-      }
-      const guestData = new Guest(guest);
-      const createdGuest = await guestData.save();
-      return createdGuest.toObject();
-    } catch (error) {
-      throw ApiError.badRequest();
+    // try {
+    const existingGuest = await Guest.findOne({
+      email: guest.email,
+    }).populate("bookedBy");
+    if (existingGuest) {
+      throw ApiError.guestExist();
     }
+    const guestData = new Guest(guest);
+    const createdGuest = await guestData.save();
+    return createdGuest.toObject();
+    // } catch (error) {
+    //   throw ApiError.badRequest();
+    // }
   }
 
   async delete(id: string): Promise<void> {
-    try {
-      await Guest.findByIdAndDelete(id);
-    } catch (error) {
-      throw ApiError.badRequest();
-    }
+    // try {
+    await Guest.findByIdAndDelete(id);
+    // } catch (error) {
+    //   throw ApiError.badRequest();
+    // }
   }
 
   async read(id: string): Promise<any | null> {
-    try {
-      const guest = await Guest.findById(id)
-        .populate({
-          path: "reservationTags", // Populate the reservationTags field
-          select: "id name categoryNameId", // Adjust the fields you want to select
-          populate: {
-            path: "categoryNameId", // Populate the categoryNameId field in reservationTags
-            select: "id name color",
-            model: "ReservationTagCategory", // Reference to the Category model
-          },
-        })
-        .populate("bookedBy");
-      return guest ? guest.toObject() : null;
-    } catch (error) {
-      throw ApiError.badRequest();
-    }
+    // try {
+    const guest = await Guest.findById(id)
+      .populate({
+        path: "reservationTags", // Populate the reservationTags field
+        select: "id name categoryNameId", // Adjust the fields you want to select
+        populate: {
+          path: "categoryNameId", // Populate the categoryNameId field in reservationTags
+          select: "id name color",
+          model: "ReservationTagCategory", // Reference to the Category model
+        },
+      })
+      .populate("bookedBy");
+    return guest ? guest.toObject() : null;
+    // } catch (error) {
+    //   throw ApiError.badRequest();
+    // }
   }
 
   async getAllGuests(): Promise<any[]> {
@@ -77,13 +77,10 @@ export class GuestDataSourceImpl implements GuestDataSource {
   }
 
   async update(id: string, guest: GuestModel): Promise<any> {
-    try {
-      const updatedGuest = await Guest.findByIdAndUpdate(id, guest, {
-        new: true,
-      }).populate("bookedBy");
-      return updatedGuest ? updatedGuest.toObject() : null;
-    } catch (error) {
-      throw ApiError.badRequest();
-    }
+    const updatedGuest = await Guest.findByIdAndUpdate(id, guest, {
+      new: true,
+    }).populate("bookedBy");
+
+    return updatedGuest ? updatedGuest.toObject() : null;
   }
 }
