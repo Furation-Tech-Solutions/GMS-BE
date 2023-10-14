86df39d54010b86c05f9aa9125c0d2deba42c560
amulshinde76 <amul.shinde@furation.tech>
2023-10-14 14:00:35 +0530

    fix: optimized the code of filter in guest and booking requests

---
+++ src/types/booking-requests-status-type.d.ts
@@ -0,0 +1,6 @@
+
+
+export interface IBookingRequestStatus {
+    name: string,
+    color: string
+}
---
+++ src/types/guest-types.d.ts
@@ -0,0 +1,10 @@
+export const ErrorMessage = {
+    NOT_FOUND: 'No matching guests found.',
+    INTERNAL_SERVER_ERROR: 'Internal server error.'
+};
+
+export const Status = {
+    PENDING: 'Pending',
+    CHECKED_IN: 'Checked In',
+    CHECKED_OUT: 'Checked Out'
+};
