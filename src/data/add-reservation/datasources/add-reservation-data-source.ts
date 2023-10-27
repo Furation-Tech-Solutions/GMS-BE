b49e78ec929788d2e99bf449082fd2397968788c
satan-furation <137980612+satan-furation@users.noreply.github.com>
2023-10-27 16:17:22 +0530

    Feature/filtres date 14 (#205)
    * fix: fix response of reservation client

--- src/data/add-reservation/datasources/add-reservation-data-source.ts
+++ src/data/add-reservation/datasources/add-reservation-data-source.ts
@@ -122,7 +122,7 @@ export class AddReservationDataSourceImpl implements AddReservationDataSource {
       })
       .populate({
         path: "client",
-        select: "id firstName lastName phone email isClient",
+        select: "id salutation firstName lastName phone email gender isClient",
       })
       .populate({
         path: "table",
