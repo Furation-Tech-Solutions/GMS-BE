848f187caacb5d36b31595be6cd5de4a31ae6003
amulshinde76 <136080668+amulshinde76@users.noreply.github.com>
2023-11-08 12:32:54 +0530

    Feature/setupoutlet (#228)
    * fix: fixed the issue of booked by name
    
    ---------
    
    Co-authored-by: satan-furation <satan.sharma@furation.tech>

--- src/presentation/middlewares/notification/notification-middleware-backend.ts
+++ src/presentation/middlewares/notification/notification-middleware-backend.ts
@@ -56,7 +56,7 @@ export async function sendNotification(title: string) {
               });
 
             });
-  
+    
             const failures = response.responses.filter((result: any) => !result.success);
   
             failures.forEach((result: any) => {
