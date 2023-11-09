739a364eecd167cd5dc73587f963e0c1b03e408c
amulshinde76 <amul.shinde@furation.tech>
2023-11-09 15:18:31 +0530

    fix: fixed the logger api for logs

--- src/data/logger/datasource/logger-datasource.ts
+++ src/data/logger/datasource/logger-datasource.ts
@@ -1,69 +1,33 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// import ApiError from "@presentation/error-handling/api-error";
-// import { LogModel } from "../models/logger-model";
+import ApiError from "@presentation/error-handling/api-error";
+import { LogModel } from "../models/logger-model";
+import { LoggerModel } from "@domain/logger/entities/logger-entity";
+import { IlogsFilter } from "types/logger/logger-schema-type";
 
-// // Create CLientTagCategoryDataSource Interface
-// export interface LoggerDataSource {
-//   create(loggerData: LoggerModel): Promise<any>;
-// //   update(id: string, tagCategory: LoggerModel): Promise<any>;
-// //   delete(id: string): Promise<void>;
-// //   read(id: string): Promise<any | null>;
-// //   getAll(loggerid: string): Promise<any[]>;
-// }
+// Create CLientTagCategoryDataSource Interface
+export interface LoggerDataSource {
+  create(loggerData: LoggerModel): Promise<any>;
+  getAll(filter: IlogsFilter): Promise<any[]>;
+} 
 
-// // TagCategory Data Source communicates with the database
-// export class LoggerDataSourceImpl
-//   implements LoggerDataSource
-// {
-//   constructor(private db: mongoose.Connection) {}
 
-//   async create(loggerData: LoggerModel): Promise<any> {
+// TagCategory Data Source communicates with the database
+export class LoggerDataSourceImpl
+  implements LoggerDataSource
+{
+  constructor(private db: mongoose.Connection) {}
 
-//     const clientTagCategoryData = new LogModel(loggerData);
-//     const createdClientTagCategory = await clientTagCategoryData.save();
-//     return createdClientTagCategory.toObject();
-//   }
+  async create(loggerData: LoggerModel): Promise<any> {
 
-// //   async delete(id: string): Promise<void> {
-// //     await ClientTagCategory.findByIdAndDelete(id);
-// //   }
+    const clientTagCategoryData = new LogModel(loggerData);
+    const createdClientTagCategory = await clientTagCategoryData.save();
+    return createdClientTagCategory.toObject();
+  }
 
-// //   async read(id: string): Promise<any | null> {
-// //     const clientTagCategory = await ClientTagCategory.findById(id).populate({
-// //       path: "tags",
-// //       select: "id name",
-// //     });
-// //     //   .populate("tags");
-// //     return clientTagCategory ? clientTagCategory.toObject() : null; // Convert to a plain JavaScript object before returning
-// //   }
 
-// //   async getAll(outletId: string): Promise<any[]> {
-// //     const clientTagCategories = await ClientTagCategory.find({
-// //       outletId: outletId,
-// //     }).populate({
-// //       path: "tags",
-// //       select: "id name",
-// //     });
-// //     //   .populate("tags");
-// //     return clientTagCategories.map((clientTagCategory) =>
-// //       clientTagCategory.toObject()
-// //     ); // Convert to plain JavaScript objects before returning
-// //   }
-
-// //   async update(
-// //     id: string,
-// //     clientTagCategory: ClientTagCategoryModel
-// //   ): Promise<any> {
-// //     const updatedClientTagCategory = await ClientTagCategory.findByIdAndUpdate(
-// //       id,
-// //       clientTagCategory,
-// //       {
-// //         new: true,
-// //       }
-// //     );
-// //     return updatedClientTagCategory
-// //       ? updatedClientTagCategory.toObject()
-// //       : null; // Convert to a plain JavaScript object before returning
-// //   }
-// }
+  async getAll(filter: IlogsFilter): Promise<any[]> {
+    const getAllLogs = await LogModel.find(filter);
+    return getAllLogs.map((logs) => logs.toObject() );
+  }
+}
