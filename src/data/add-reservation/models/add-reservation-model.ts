a8d1f2046c69b28aba217f95babc3d17eb078320
satan-furation <137980612+satan-furation@users.noreply.github.com>
2023-11-25 17:28:35 +0530

    Feature/filtres date 14 (#247)
    * fix: fix all status codes according to operations
    
    * fix: fix all change status code create and delete
    
    * fix: write proper commit about functions in reservation data source file
    
    ---------
    
    Co-authored-by: amulshinde76 <amul.shinde@furation.tech>
    Co-authored-by: amulshinde76 <136080668+amulshinde76@users.noreply.github.com>

--- src/data/add-reservation/models/add-reservation-model.ts
+++ src/data/add-reservation/models/add-reservation-model.ts
@@ -1,25 +1,30 @@
+// Importing Mongoose library for schema creation
 import mongoose from "mongoose";
 
+// Creating a schema for adding reservations
 const addReservationSchema = new mongoose.Schema({
-  // Availability
+  // Date of the reservation
   date: {
     type: String,
     trim: true,
     required: [true, "Please select the Date"],
   },
 
+  // Number of guests for the reservation
   noOfGuests: {
     type: Number,
-    required: [true, "Please select the Date"],
+    required: [true, "Please specify the number of Guests"],
     default: 1,
   },
 
+  // Associated shift for the reservation
   shift: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Shift",
     required: [true, "Please select the Shift"],
   },
 
+  // Duration of the reservation
   duration: {
     type: String,
     trim: true,
@@ -27,57 +32,72 @@ const addReservationSchema = new mongoose.Schema({
     required: [true, "Please select the Duration"],
   },
 
+  // Seating area for the reservation
   seatingArea: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "SeatingArea",
     required: [true, "Please select the Seating Area"],
   },
 
+  // Time slot for the reservation
   timeSlot: {
     type: String,
     trim: true,
     required: [true, "Please select the Time Slot"],
   },
 
+  // Associated client for the reservation
   client: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Client",
     required: [true, "Please select the Client"],
   },
+
+  // Tags associated with the reservation
   reservationTags: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "ReservationTag",
     },
   ],
+
+  // Additional notes for the reservation
   reservationNote: {
     type: String,
-    maxlength: [2000, "Last name should have less than 2000 characters"],
+    maxlength: [2000, "Note should have less than 2000 characters"],
     trim: true,
     default: "",
   },
+
+  // Status of the reservation
   reservationStatus: {
     type: String,
     trim: true,
     default: "unassigned",
   },
 
+  // Table(s) allocated for the reservation
   table: [
     {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Table",
-    required: [false, "Please select the Table"],
-  }
-],
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Table",
+      required: [false, "Please select the Table"],
+    },
+  ],
+
+  // Person who booked the reservation
   bookedBy: {
     type: String,
     default: "",
   },
 
+  // Server assigned to the reservation
   serverName: {
     type: String,
     default: "",
   },
+
+  // Perks offered for the reservation
   perks: {
     type: String,
     maxlength: [500, "Perks should have less than 500 characters"],
@@ -85,41 +105,55 @@ const addReservationSchema = new mongoose.Schema({
     default: "",
   },
 
+  // Confirmation mail sending status
   confirmationMailSending: {
     type: Boolean,
     default: false,
   },
+
+  // Pre-payment made for the reservation
   prePayment: {
     type: Number,
-    required: [false, "Please fill pre payment"],
+    required: [false, "Please fill in pre-payment"],
     default: 0,
   },
+
+  // On-site payment made for the reservation
   onSitePayment: {
     type: Number,
-    required: [false, "Please fill onsite payment"],
+    required: [false, "Please fill in on-site payment"],
     default: 0,
   },
+
+  // Associated outlet ID for the reservation
   outletId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Outlet",
     default: null,
   },
+
+  // User who last updated the reservation
   updatedBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "UserAccount",
     default: "",
   },
+
+  // User who created the reservation
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "UserAccount",
     default: "",
   },
+
+  // Timestamp for the creation of the reservation
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
 
+// Creating a Mongoose model 'AddReservation' using the defined schema
 export const AddReservation = mongoose.model(
   "AddReservation",
   addReservationSchema
